f1c8903fd95dfe51817d438d8be01ecefa7942fe
Matteus Moreno <matteus_moreno@live.com>
2025-05-03 11:55:33 -0300

    configuring database (MySQL)

---
+++ src/main/java/id/com/anisatrilestari/NisaPhoneApplication.java
@@ -0,0 +1,13 @@
+package id.com.anisatrilestari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisaPhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NisaPhoneApplication.class, args);
+	}
+
+}
